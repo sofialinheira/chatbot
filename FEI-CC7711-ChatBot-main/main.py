718de6859344b90e6408b2d1e6b5b14f8f57888d
sofialinheira <77805417+sofialinheira@users.noreply.github.com>
2023-09-01 20:47:17 -0300

    Add files via upload

--- FEI-CC7711-ChatBot-main/main.py
+++ FEI-CC7711-ChatBot-main/main.py
@@ -5,17 +5,17 @@ myChatBot = ChatBot()
 #criar o modelo
 myChatBot.createModel()
 
-print("Bem vindo ao Chatbot")
+print('\033[1;35m'+"Bem vindo ao Chatbot"+'\033[0;0m')
 
 
 pergunta = input("como posso te ajudar?")
 resposta, intencao = myChatBot.chatbot_response(pergunta)
-print(resposta + "   ["+intencao[0]['intent']+"]")
+print('\033[34m'+resposta + "   ["+intencao[0]['intent']+"]"+'\033[0;0m')
 
 
 while (intencao[0]['intent']!="despedida"):
     pergunta = input("posso lhe ajudar com algo a mais?")
     resposta, intencao = myChatBot.chatbot_response(pergunta)
-    print(resposta + "   [" + intencao[0]['intent'] + "]")
+    print('\033[34m'+resposta + "   ["+intencao[0]['intent']+"]"+'\033[0;0m')
 
 print("Foi um prazer atendê-lo")
